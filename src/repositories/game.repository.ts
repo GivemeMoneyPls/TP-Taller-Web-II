682bda94683a756abd226c33c7fb468f5bfbbb0c
Agustin <agustinfederico6@gmail.com>
2025-10-28 19:34:27 -0300

    actualizacion base de datos y metodos de repositorio

--- src/repositories/game.repository.ts
+++ src/repositories/game.repository.ts
@@ -3,11 +3,16 @@ import { prisma } from "../prisma.js";
 export class GameRepository {
 
     async findAllGames() {
-        return await prisma.game.findMany();;
+        return await prisma.juego.findMany({
+            include: { plataforma: true, 
+                juego_genero: { include: { genero: true } } }
+        });
     }
 
     async findGameById(id: number) {
-        return await prisma.game.findUnique({
+        return await prisma.juego.findUnique({
+            include: { plataforma: true, 
+                juego_genero: { include: { genero: true } } },
             where: { id : id }
         });
     }
