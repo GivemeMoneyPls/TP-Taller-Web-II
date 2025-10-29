c4d689718138ff9543bf5c757a6a383aa70e674c
Agustin <agustinfederico6@gmail.com>
2025-10-29 17:40:22 -0300

    cambio a cors para poder acceder a las peticiones http desde el front

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express, {type Request, type Response} from 'express';
 import { AppRoutes } from './routes/routes.js';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:4200', // solo tu frontend Angular
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(AppRoutes.routes);
 
